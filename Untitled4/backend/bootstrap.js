ef1fedbfe36d63a3b7710de7b1502af60a7daf25
SebCourvoisier <sebastien.courvoisier@wakanda.io>
2016-06-21 10:39:02 +0200

    Test 1

--- Untitled4/backend/bootstrap.js
+++ Untitled4/backend/bootstrap.js
@@ -2,3 +2,5 @@
  * A BootStrap is a JavaScript file that is run when your project is loaded on the server.
  * You can use it  to initialize your application, define HTTP pattern handlers etc..
  */
+ 
+ // nope
